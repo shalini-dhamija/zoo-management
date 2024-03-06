efb71ef1fde2b65bd94fa26a3e71010109f2f80e
Anastasia C <clarkanastasia@outlook.com>
2024-03-06 15:43:46 +0000

    add seed data

--- Zoo.cs
+++ Zoo.cs
@@ -44,5 +44,55 @@ protected override void OnModelCreating(ModelBuilder modelBuilder)
             DateOfAcquisition = new DateTime(2001, 2, 3).ToUniversalTime(),
         };
         modelBuilder.Entity<Animal>().HasData(simba, nala);
+
+        var species = new Dictionary<string, Classification>(){
+            {"elephant", Classification.Mammal},
+            {"zebra", Classification.Mammal},
+            {"snake", Classification.Reptile},
+            {"giraffe", Classification.Mammal},
+            {"spider", Classification.Insect},
+            {"hippo", Classification.Mammal},
+            {"parrot", Classification.Bird},
+            {"goldfish", Classification.Fish}
+        };
+        var speciesId = 1;
+        foreach (var s in species){
+            var newSpecies = new Species
+            {
+                Id = speciesId,
+                Name = s.Key,
+                Classification = s.Value,
+            };
+            modelBuilder.Entity<Species>().HasData(newSpecies);
+            speciesId++;
+        }
+        
+        var names = new[]
+        {
+            "Spot", "Pooh", "Piglet", "Kanga", "Peter", "Tabatha", "Winston", "Daffy", "Garfield", "Mickey", "Snoopy"
+        };
+
+        var arrSpeciesId = new[]
+            {
+                -1, 1, 2, 3, 4, 5, 6, 7
+            };
+        
+        var values = Enum.GetValues(typeof(Sex));
+        var random = new Random();
+
+        var newAnimals = Enumerable.Range(1, 100).Select(index =>
+            new Animal{
+                Id = index,
+                Name = names[Random.Shared.Next(names.Length)],                
+                Sex = (Sex)values.GetValue(random.Next(values.Length)),
+                DateOfAcquisition = DateTime.Now.ToUniversalTime(),
+                DateOfBirth = DateTime.Now.ToUniversalTime(),
+                SpeciesId = arrSpeciesId[Random.Shared.Next(arrSpeciesId.Length)],
+            }).ToArray();
+
+        foreach (var animal in newAnimals)
+        {
+            modelBuilder.Entity<Animal>().HasData(animal);
+        }    
     }
 }
